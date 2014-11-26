6f39b90d0bd1031a7f158feae5b79ca4e03fa85d
n.horner <n.horner@har.mrc.ac.uk>
2014-11-26 13:00:17 +0000

    commit before branching

--- js/view.js
+++ js/view.js
@@ -7,6 +7,56 @@ function SliceView(){
 	this.setContainer = function(container_div){
 		//alert(container_div);
 	};
+
+
+
+	this.setup = function() {
+		// create the 2D renderers
+		// .. for the X orientation
+		sliceX = new X.renderer2D();
+		sliceX.container = 'sliceX';
+		sliceX.orientation = 'X';
+		sliceX.init();
+
+		// .. for Y
+		var sliceY = new X.renderer2D();
+		sliceY.container = 'sliceY';
+		sliceY.orientation = 'Y';
+		sliceY.init();
+
+		// .. and for Z
+		sliceZ = new X.renderer2D();
+		sliceZ.container = 'sliceZ';
+		sliceZ.orientation = 'Z';
+		sliceZ.init();
+		
+
+		// create the 2D renderers
+		// .. for the X orientation
+		sliceX = new X.renderer2D();
+		sliceX.container = 'sliceX';
+		sliceX.orientation = 'X';
+		sliceX.init();
+
+		// .. for Y
+		var sliceY = new X.renderer2D();
+		sliceY.container = 'sliceY';
+		sliceY.orientation = 'Y';
+		sliceY.init();
+
+		// .. and for Z
+		sliceZ = new X.renderer2D();
+		sliceZ.container = 'sliceZ';
+		sliceZ.orientation = 'Z';
+		sliceZ.init(); 
+
+  
+
+	};
+
+  
+  
+  
 }
 
 
