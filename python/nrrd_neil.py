83baca7f0fbf47c5eadca6511af0400e23b90bc7
n.horner <n.horner@har.mrc.ac.uk>
2015-02-27 16:18:23 +0000

    H5pyt kind of working put can't do on-disk transposes. Switching to numpy memory mapping

---
+++ python/nrrd_neil.py
@@ -0,0 +1,303 @@
+#!/usr/bin/env python
+"""
+Adapted from https://github.com/mhe/pynrrd/  Please see licence at https://github.com/mhe/pynrrd/blob/master/LICENSE
+Copyright (c) 2012 Maarten H. Everts and contributors.
+
+"""
+
+import numpy as np
+import os.path
+import os
+from datetime import datetime
+
+class NrrdError(Exception):
+    """Exceptions for Nrrd class."""
+    pass
+
+
+_TYPEMAP_NRRD2NUMPY = {
+    'signed char': 'i1',
+    'int8': 'i1',
+    'int8_t': 'i1',
+    'uchar': 'u1',
+    'unsigned char': 'u1',
+    'uint8': 'u1',
+    'uint8_t': 'u1',
+    'short': 'i2',
+    'short int': 'i2',
+    'signed short': 'i2',
+    'signed short int': 'i2',
+    'int16': 'i2',
+    'int16_t': 'i2',
+    'ushort': 'u2',
+    'unsigned short': 'u2',
+    'unsigned short int': 'u2',
+    'uint16': 'u2',
+    'uint16_t': 'u2',
+    'int': 'i4',
+    'signed int': 'i4',
+    'int32': 'i4',
+    'int32_t': 'i4',
+    'uint': 'u4',
+    'unsigned int': 'u4',
+    'uint32': 'u4',
+    'uint32_t': 'u4',
+    'longlong': 'i8',
+    'long long': 'i8',
+    'long long int': 'i8',
+    'signed long long': 'i8',
+    'signed long long int': 'i8',
+    'int64': 'i8',
+    'int64_t': 'i8',
+    'ulonglong': 'u8',
+    'unsigned long long': 'u8',
+    'unsigned long long int': 'u8',
+    'uint64': 'u8',
+    'uint64_t': 'u8',
+    'float': 'f4',
+    'double': 'f8',
+    'block': 'V'
+}
+
+_TYPEMAP_NUMPY2NRRD = {
+    'i1': 'int8',
+    'u1': 'uint8',
+    'i2': 'int16',
+    'u2': 'uint16',
+    'i4': 'int32',
+    'u4': 'uint32',
+    'i8': 'int64',
+    'u8': 'uint64',
+    'f4': 'float',
+    'f8': 'double',
+    'V': 'block'
+}
+
+_NUMPY2NRRD_ENDIAN_MAP = {
+    '<': 'little',
+    'L': 'little',
+    '>': 'big',
+    'B': 'big'
+}
+
+
+
+_NRRD_REQUIRED_FIELDS = ['dimension', 'type', 'encoding', 'sizes']
+
+# The supported field values
+_NRRD_FIELD_ORDER = [
+    'type',
+    'dimension',
+    'space dimension',
+    'space',
+    'sizes',
+    'space directions',
+    'kinds',
+    'endian',
+    'encoding',
+    'min',
+    'max',
+    'oldmin',
+    'old min',
+    'oldmax',
+    'old max',
+    'content',
+    'sample units',
+    'spacings',
+    'thicknesses',
+    'axis mins',
+    'axismins',
+    'axis maxs',
+    'axismaxs',
+    'centerings',
+    'labels',
+    'units',
+    'space units',
+    'space origin',
+    'measurement frame',
+    'data file']
+
+
+def _format_nrrd_list(fieldValue) :
+    return ' '.join([str(x) for x in fieldValue])
+
+
+def _format_nrrdvector(v) :
+    return '(' + ','.join([str(x) for x in v]) + ')'
+
+
+def _format_optional_nrrdvector(v):
+    if (v == 'none') :
+        return 'none'
+    else :
+        return _format_nrrdvector(v)
+
+_NRRD_FIELD_FORMATTERS = {
+    'dimension': str,
+    'type': str,
+    'sizes': _format_nrrd_list,
+    'endian': str,
+    'encoding': str,
+    'min': str,
+    'max': str,
+    'oldmin': str,
+    'old min': str,
+    'oldmax': str,
+    'old max': str,
+    'lineskip': str,
+    'line skip': str,
+    'byteskip': str,
+    'byte skip': str,
+    'content': str,
+    'sample units': str,
+    'datafile': str,
+    'data file': str,
+    'spacings': _format_nrrd_list,
+    'thicknesses': _format_nrrd_list,
+    'axis mins': _format_nrrd_list,
+    'axismins': _format_nrrd_list,
+    'axis maxs': _format_nrrd_list,
+    'axismaxs': _format_nrrd_list,
+    'centerings': _format_nrrd_list,
+    'labels': _format_nrrd_list,
+    'units': _format_nrrd_list,
+    'kinds': _format_nrrd_list,
+    'space': str,
+    'space dimension': str,
+    'space units': _format_nrrd_list,
+    'space origin': _format_nrrdvector,
+    'space directions': lambda fieldValue: ' '.join([_format_optional_nrrdvector(x) for x in fieldValue]),
+    'measurement frame': lambda fieldValue: ' '.join([_format_optional_nrrdvector(x) for x in fieldValue]),
+}
+
+
+def _write_data(data, filehandle, options=None):
+    """
+    :param data: numpy ndarray (memmaped if working with large files)
+    :param filehandle:
+    :param options: dict
+    :return:
+    Write a nrrd from a memmaped ndarray. Writes a slice at a time to prevent hogging RAM
+    """
+    for z in range(data.shape[2]):
+        rawdata = data[:, :, z].tostring(order='F')
+        filehandle.write(rawdata)
+
+
+def write(filename, data, options={}, separate_header=False):
+    """Write the numpy data to a nrrd file. The nrrd header values to use are
+    inferred from from the data. Additional options can be passed in the
+    options dictionary. See the read() function for the structure of this
+    dictionary.
+
+    To set data samplings, use e.g. `options['spacings'] = [s1, s2, s3]` for
+    3d data with sampling deltas `s1`, `s2`, and `s3` in each dimension.
+
+    """
+    # Infer a number of fields from the ndarray and ignore values
+    # in the options dictionary.
+    options['type'] = _TYPEMAP_NUMPY2NRRD[data.dtype.str[1:]]
+    if data.dtype.itemsize > 1:
+        options['endian'] = _NUMPY2NRRD_ENDIAN_MAP[data.dtype.str[:1]]
+    # if 'space' is specified 'space dimension' can not. See http://teem.sourceforge.net/nrrd/format.html#space
+    if 'space' in options.keys() and 'space dimension' in options.keys():
+        del options['space dimension']
+    options['dimension'] = data.ndim
+    options['sizes'] = list(data.shape)
+
+    # The default encoding is 'gzip'
+    if 'encoding' not in options:
+        options['encoding'] = 'raw'
+
+    with open(filename,'wb') as filehandle:
+        filehandle.write('NRRD0004\n')
+        filehandle.write('# This NRRD file was generated by HARP with the help of pynrrd\n')
+        filehandle.write('# on ' +
+                         datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S') +
+                         '(GMT).\n')
+        filehandle.write('# Complete NRRD file format specification at:\n');
+        filehandle.write('# http://teem.sourceforge.net/nrrd/format.html\n');
+
+        # Write the fields in order, this ignores fields not in _NRRD_FIELD_ORDER
+        for field in _NRRD_FIELD_ORDER:
+            if options.has_key(field):
+                outline = (field + ': ' +
+                           _NRRD_FIELD_FORMATTERS[field](options[field]) +
+                           '\n')
+                filehandle.write(outline)
+        for (k,v) in options.get('keyvaluepairs', {}).items():
+            outline = str(k) + ':=' + str(v) + '\n'
+            filehandle.write(outline)
+
+        # Write the closing extra newline
+        filehandle.write('\n')
+
+        # If a single file desired, write data
+        if not separate_header:
+            _write_data(data, filehandle, options)
+
+
+def write_nrrd_header(filehandle, shape, npdtype, ndim=3, options={}):
+    """Write the numpy data to a nrrd file. The nrrd header values to use are
+    inferred from from the data. Additional options can be passed in the
+    options dictionary. See the read() function for the structure of this
+    dictionary.
+
+    To set data samplings, use e.g. `options['spacings'] = [s1, s2, s3]` for
+    3d data with sampling deltas `s1`, `s2`, and `s3` in each dimension.
+
+    """
+    # Infer a number of fields from the ndarray and ignore values
+    # in the options dictionary.
+
+    #'make sure file does not exist as we are appending to it
+    # if os.path.isfile(filename):
+    #     os.remove(filename)
+
+
+
+    options['type'] = _TYPEMAP_NUMPY2NRRD[npdtype.str[1:]]
+    if npdtype.itemsize > 1:
+        options['endian'] = _NUMPY2NRRD_ENDIAN_MAP[npdtype.str[:1]]
+    # if 'space' is specified 'space dimension' can not. See http://teem.sourceforge.net/nrrd/format.html#space
+    if 'space' in options.keys() and 'space dimension' in options.keys():
+        del options['space dimension']
+    options['dimension'] = ndim
+    options['sizes'] = list(shape)
+
+    # The default encoding is 'gzip'
+    if 'encoding' not in options:
+        options['encoding'] = 'raw'
+
+    filehandle.write('NRRD0004\n')
+    filehandle.write('# This NRRD file was generated by HARP with the help of pynrrd\n')
+    filehandle.write('# on ' +
+                     datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S') +
+                     '(GMT).\n')
+    filehandle.write('# Complete NRRD file format specification at:\n');
+    filehandle.write('# http://teem.sourceforge.net/nrrd/format.html\n');
+
+    # Write the fields in order, this ignores fields not in _NRRD_FIELD_ORDER
+    for field in _NRRD_FIELD_ORDER:
+        if options.has_key(field):
+            outline = (field + ': ' +
+                       _NRRD_FIELD_FORMATTERS[field](options[field]) +
+                       '\n')
+            filehandle.write(outline)
+    for (k,v) in options.get('keyvaluepairs', {}).items():
+        outline = str(k) + ':=' + str(v) + '\n'
+        filehandle.write(outline)
+
+    # Write the closing extra newline
+    filehandle.write('\n')
+
+    # If a single file desired, write data
+    return filehandle
+    #_write_data(data, filehandle, options)
+
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
--- python/test.py
+++ python/test.py
@@ -1,10 +1,17 @@
 
-import nose
+import nrrd
 import conversion
 import os
-from os.path import expanduser
 import cv2
 import h5py
+import tempfile
+import sys
+import numpy as np
+
+if sys.platform == "win32" or sys.platform == "win64":
+    windows = True
+else:
+    windows = False
 
 #use @with_setup(setup_func, teardown_func)
 
@@ -26,19 +33,64 @@ def test_minc_to_array():
     mapped_array = minc['image']['0']['image']
     #print type(mapped_array[10, 10, 10])
 
-    xyscaledH5 = h5py.File('tempXyscaledH5', 'a')
+    TEMP_XY = 'tempXYscaledH5'
+
+    if os.path.isfile(TEMP_XY):
+        os.remove(TEMP_XY)
+
+    xyscaledH5 = h5py.File(TEMP_XY, 'a')
 
     # We need to get the shape of the resized array
-    xyshrunk_shape = list(cv2.resize(mapped_array[1, :, :], None, fx=2, fy=2).shape)
+    xyshrunk_shape = list(cv2.resize(mapped_array[1, :, :], None, fx=0.5, fy=0.5).shape)
     xyshrunk_shape.insert(0, mapped_array.shape[0])
+
     dtype = mapped_array.dtype
 
-    xyscaledH5.create_dataset('tempscale', xyshrunk_shape, dtype)
+    xy_dset = xyscaledH5.create_dataset('tempscale', xyshrunk_shape, dtype)
 
     for i in range(0, mapped_array.shape[0]):
 
-        shrunk_slice = cv2.resize(mapped_array[i, :, :], None, fx=2, fy=2)
+        shrunk_slice = cv2.resize(mapped_array[i, :, :], None, fx=0.5, fy=0.5)
 
-        xyscaledH5[i, :, :] = shrunk_slice
+        xy_dset[i, ] = shrunk_slice
+
+    #scale in xz
+    TEMP_XYZ = 'tempXYZscaledRaw'
+
+    if os.path.isfile(TEMP_XYZ):
+        os.remove(TEMP_XYZ)
+
+    xyzshrunk_shape = list(cv2.resize(xy_dset[:, :, 1], None, fx=1.0, fy=0.5).shape)
+    xyzshrunk_shape.insert(2, xy_dset.shape[2])
+
+    #xyzscaledH5 = h5py.File(TEMP_XYZ, 'a')
+
+    temp_xyz = tempfile.TemporaryFile(mode='wb+')
+    #xyz_dset = xyzscaledH5.create_dataset('tempscaleXYZ', xyzshrunk_shape, dtype)
+
+    for i in range(0, xy_dset.shape[2]):
+
+        shrunk_slice = cv2.resize(xy_dset[:, :, i], None, fx=1.0, fy=0.5)
+        if windows:
+            shrunk_slice.tofile(temp_xyz.file)
+        else:
+            shrunk_slice.tofile(temp_xyz)
+
+    xyz_scaled_mmap = np.memmap(temp_xyz, dtype=dtype, mode='r', shape=tuple(xyzshrunk_shape))
+
+    nrrdout = 'scaled_nrrd_temp.nrrd'
+    if os.path.isfile(nrrdout):
+        os.remove(nrrdout)
+
+    nrrd.write('scaled_nrrd_temp.nrrd', np.swapaxes(xyz_scaled_mmap, 0, 1 )
 
 
+
+#create memory mapped version of the temporary xy scaled slices
+
+
+    #nrrd.write(outpath, np.swapaxes(xyz_scaled_mmap.T, 1, 2))
+
+
+if __name__ == '__main__':
+    test_minc_to_array()
