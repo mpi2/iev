d06328d67c9000c10f0ea7f2f4087f620c17f152
James Brown <james.brown@har.mrc.ac.uk>
2015-03-02 17:01:30 +0000

    Created slice generators for tiffs, nrrds and mincs

---
+++ python/sql/insert_test_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO phenodcc_media.media_file (cid, lid, gid, pid, qid, sid, phase_id, url)
+VALUES
+(4, 8, 0, 203, 5439, 35, 2, "opt.nrrd.bz2"),
+(4, 8, 0, 203, 5439, 35, 2, "uCT_14_5.nrrd.bz2"),
+(4, 8, 0, 203, 5439, 35, 2, "uCT_18_5.nrrd.bz2"),
+(4, 8, 0, 203, 5439, 35, 2, "uCT_15_5.mnc"),
+(4, 8, 0, 203, 5439, 35, 2, "uCT_14_5.zip")
